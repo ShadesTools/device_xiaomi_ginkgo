8e871ff5f5fd0741885fd32e38e5be07055e566d
Kitsunejasutin <justineryandeguia@yahoo.com>
2021-06-26 13:54:26 +0800

    ginkgo: Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,18 +1,29 @@
 rm -rf hardware/qcom-caf/sm8150/audio
 rm -rf hardware/qcom-caf/sm8150/display
 rm -rf hardware/qcom-caf/sm8150/media
-rm -rf vendor/qcom/opensource/commonsys-intf/bluetooth
-rm -rf vendor/qcom/opensource/commonsys/bluetooth_ext
-rm -rf vendor/qcom/opensource/commonsys/packages/apps/Bluetooth
-rm -rf vendor/qcom/opensource/commonsys/system/bt
-rm -rf system/bt
-rm -rf packages/apps/Bluetooth
+rm -rf vendor/qcom/opensource/power
+rm -rf vendor/qcom/opensource/interfaces
+rm -rf hardware/broadcom/nfc
+rm -rf hardware/nxp/nfc
+rm -rf packages/apps/Nfc
+rm -rf vendor/nxp/opensource/interfaces/nfc
+rm -rf vendor/nxp/opensource/commonsys/external/libnfc-nci
+rm -rf vendor/nxp/opensource/commonsys/frameworks
+rm -rf vendor/nxp/opensource/commonsys/packages/apps/Nfc
+rm -rf vendor/nxp/opensource/sn100x/halimpl
+rm -rf vendor/nxp/opensource/sn100x/hidlimpl
 git clone https://github.com/ArrowOS/android_hardware_qcom_display.git -b arrow-11.0-caf-sm8150 hardware/qcom-caf/sm8150/display
 git clone https://github.com/ArrowOS/android_hardware_qcom_audio.git -b arrow-11.0-caf-sm8150 hardware/qcom-caf/sm8150/audio
 git clone https://github.com/ArrowOS/android_hardware_qcom_media.git -b arrow-11.0-caf-sm8150 hardware/qcom-caf/sm8150/media
-git clone https://github.com/LineageOS/android_vendor_qcom_opensource_bluetooth-commonsys-intf.git vendor/qcom/opensource/commonsys-intf/bluetooth 
-git clone https://github.com/LineageOS/android_vendor_qcom_opensource_bluetooth_ext.git vendor/qcom/opensource/commonsys/bluetooth_ext
-git clone https://github.com/LineageOS/android_vendor_qcom_opensource_packages_apps_Bluetooth.git vendor/qcom/opensource/commonsys/packages/apps/Bluetooth
-git clone https://github.com/LineageOS/android_vendor_qcom_opensource_system_bt.git vendor/qcom/opensource/commonsys/system/bt
-git clone https://github.com/LineageOS/android_system_bt.git system/bt
-git clone https://github.com/LineageOS/android_packages_apps_Bluetooth.git packages/apps/Bluetooth
+git clone https://github.com/LineageOS/android_hardware_broadcom_nfc.git hardware/broadcom/nfc
+git clone https://github.com/LineageOS/android_hardware_nxp_nfc.git hardware/nxp/nfc
+git clone https://github.com/LineageOS/android_packages_apps_Nfc.git packages/apps/Nfc
+git clone https://github.com/LineageOS/android_vendor_nxp_interfaces_opensource_nfc.git vendor/nxp/opensource/interfaces/nfc
+git clone https://github.com/LineageOS/android_vendor_nxp_opensource_external_libnfc-nci.git vendor/nxp/opensource/commonsys/external/libnfc-nci
+git clone https://github.com/LineageOS/android_vendor_nxp_opensource_frameworks.git vendor/nxp/opensource/commonsys/frameworks
+git clone https://github.com/LineageOS/android_vendor_nxp_opensource_packages_apps_Nfc.git vendor/nxp/opensource/commonsys/packages/apps/Nfc
+git clone https://github.com/LineageOS/android_vendor_nxp_opensource_halimpl.git -b lineage-18.1-sn100x vendor/nxp/opensource/sn100x/halimpl
+git clone https://github.com/LineageOS/android_vendor_nxp_opensource_hidlimpl.git -b lineage-18.1-sn100x vendor/nxp/opensource/sn100x/hidlimpl
+git clone https://github.com/LineageOS/android_external_libnfc-nxp external/libnfc-nxp
+git clone https://github.com/ArrowOS/android_vendor_qcom_opensource_power.git vendor/qcom/opensource/power
+git clone https://github.com/ArrowOS/android_vendor_qcom_opensource_interfaces.git vendor/qcom/opensource/interfaces
